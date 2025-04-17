ecb1b587a78d9d163bf5dc636c70e1aecd1dd91f
gary <gary>
2025-04-17 14:58:12 +0800

    rendered and updated 04-17-2025-14-58

--- execgroups/_config0_configs/ubuntu/_rules/order.py
+++
@@ -1,8 +0,0 @@
-def default():
-
-    ordered_tasks= ["15-add_ansible_sshkey.py,end",
-                    "20-add_ansible_host.py,end",
-                    "30-exec_ansible.py,end"]
-
-    return ordered_tasks
-
